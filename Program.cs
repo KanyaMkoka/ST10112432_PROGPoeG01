1b60bd7e469e498e0bf5626940c94f5c673044d2
ST10112432 Kanya Mkoka <ST10112432@vcconnect.edu.za>
2024-04-16 22:39:23 +0200

    Referencing

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿/* Name: Kanya Mkoka
  * Student Number: ST10112432
  * Group: 01
- * I acknowledge bits of this code is not my own work and has been extracted from https://chat.openai.com/c/38ab336e-fa71-4664-9ee5-b5cc60bf834f 
- * and https://stackoverflow.blog/2022/06/15/c-ienumerable-yield-return-and-lazy-evaluation/
+ * I acknowledge bits of this code is not my own work and has been extracted from https://chat.openai.com/c/38ab336e-fa71-4664-9ee5-b5cc60bf834f, 
+ * https://www.youtube.com/watch?v=67oWw9TanOk&list=PL82C6-O4XrHfoN_Y4MwGvJz5BntiL0z0D and https://stackoverflow.blog/2022/06/15/c-ienumerable-yield-return-and-lazy-evaluation/
  */
 using Microsoft.SqlServer.Server;
 using ST10112432_PROGPart1.Classes;
