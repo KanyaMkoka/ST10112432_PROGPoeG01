88eeedd71b8c95dc8352a4f56ced862196735db0
lab_services_student <lab_services_student@labVMH8OX>
2024-04-16 10:57:57 +0200

    Add project files.

---
+++ Classes/Ingredient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10112432_PROGPart1
+{
+    public class Ingredient //defining a class
+    {
+        //properties storing Name, Quantity and Unit of the ingredient measurement
+        public string Name { get; set; } //name of the ingredient
+        public string Quantity { get; set; } //ingredient Quantity
+        public string Unit {  get; set; } //Measurement unit of the ingredient
+
+        //initialising ingredient properties using a constructor
+        public Ingredient(string name, double quantity, string unit) 
+        { 
+            Name = name;
+            Quantity = quantity;
+            Unit = unit;
+        }
+        //ToString method
+        public override string ToString()
+        {
+            return $"{Quantity} {Unit} of {Name}";
+        }
+
+    }
+}
---
+++ Classes/Recipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10112432_PROGPart1.Classes
+{
+    public class Recipe
+    {
+        public Ingredient[] Ingredients { get; set; }
+        public string[] Procedure { get; set; }
+
+        public Recipe(int numIngredients, int numProcedure)
+        { 
+            Ingredients = new Ingredient[numIngredients];
+            Procedure = new string[numProcedure];
+        }
+    }
+}
